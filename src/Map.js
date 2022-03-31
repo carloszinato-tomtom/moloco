31a789af40b7c80a0306cfe630ac172000822a9e
Carlos Zinato <carlos.zinato@tomtom.com>
2022-03-31 16:10:12 +0200

    adds route to map

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
-import tt from '@tomtom-international/web-sdk-maps';
+import ttMap from '@tomtom-international/web-sdk-maps';
+import ttService from '@tomtom-international/web-sdk-services'
 
 const BARCELONA = [2.0701, 41.3926];
+const AMSTERDAM = [4.900437,52.379184];
 
 function Map() {
   
@@ -10,13 +12,18 @@ function Map() {
   const mapElement = useRef();
 
   useEffect(() => {
-    let map = tt.map({
+    let map = ttMap.map({
       key: "QwMM6cMMe1lyGo0jL4w8Zv5nGrerydvb",
       container: mapElement.current,
-      center: BARCELONA,
+      center: AMSTERDAM,
       zoom: 12
     });
     setMap(map);
+    map.on('load', function() {
+        map.showTrafficFlow();
+        var nav = new ttMap.NavigationControl({});
+        map.addControl(nav, 'bottom-right');
+    });
     return () => map.remove();
   }, []);
 
@@ -24,12 +31,49 @@ function Map() {
     map.setCenter([parseFloat(lat), parseFloat(lon)]);
     map.setZoom(zoom);
   };
+  
+  const getRoute = async() => {
+      const routeOptions = {
+          key: "QwMM6cMMe1lyGo0jL4w8Zv5nGrerydvb",
+          locations: '4.900437,52.379184:4.325819,52.080756'
+          // Ams centraal 4.900437,52.379184
+          // DH CS: 4.325819,52.080756
+      };
+      
+      const route = await (await ttService.services.calculateRoute(routeOptions)).toGeoJson()
+      addPolygonToMap('1', route);
+      
+  };
+
+    function addPolygonToMap(id, rangeData) {
+        let polygonLayer = buildStyle(id, rangeData);
+        map.addLayer(polygonLayer);
+    }
+
+    function buildStyle(id, data) {
+        return {
+            'id': id,
+            'type': 'line',
+            'source': {
+                'type': 'geojson',
+                'data': data
+            },
+            'paint': {
+                'line-color': '#3b73b4',
+                'line-width': 7,
+            },
+            'layout': {}
+        }
+    }
 
   return (
-    <div
-        ref={mapElement}
-        style={{width: '50vw', height: '90vh'}}
-    />
+    <>
+            <button onClick={getRoute}>Route</button>
+        <div
+            ref={mapElement}
+            style={{width: '50vw', height: '90vh'}}
+        />
+    </>
   );
 }
 
